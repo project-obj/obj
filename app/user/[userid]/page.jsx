2a2da5c514e414eb9e5efe0854fa7f1c6cdd6ad0
sunkeydokey <sunkeydokey@gmail.com>
2023-06-28 13:22:14 +0900

    마이페이지

--- app/user/[userid]/page.jsx
+++ app/user/[userid]/page.jsx
@@ -5,10 +5,14 @@ import Link from 'next/link';
 
 import UserInfo from '@/components/userinfo';
 import Cookies from 'js-cookie';
-import Paging from '@/components/pageNation';
+import Pagination from 'react-js-pagination';
 
 const page = ({ params }) => {
   const [userDatas, setUserDatas] = useState([]);
+  const [page, setPage] = useState(1);
+  const [items, setItems] = useState(10);
+  const [isLoading, setIsLoading] = useState(true);
+  console.log(userDatas);
 
   useEffect(() => {
     axios
@@ -20,14 +24,17 @@ const page = ({ params }) => {
         withCredentials: true,
       })
       .then((res) => res?.data?.Places || [])
-      .then((data) => setUserDatas([...data]));
+      .then((data) => setUserDatas([...data]))
+      .then(() => setIsLoading(false));
   }, []);
 
-  console.log(userDatas);
+  const handlePageChange = (page) => {
+    setPage(page);
+  };
 
   return (
     <div className="flex w-screen flex-col items-center justify-start">
-      <div className="flex h-[15vh] items-center">
+      <div className="flex h-[10vh] items-center">
         <Link href={'/place'}>
           <button className="my-auto box-border block rounded-md border-none border-mint bg-mint px-20 py-4 font-bold text-white hover:bg-mint/80">
             등록하러 가기!
@@ -36,20 +43,32 @@ const page = ({ params }) => {
       </div>
 
       <div className="h-[75vh] w-screen">
-        <div className="flex h-[80%] justify-around">
-          <div className="w-[45vw] flex-col items-center justify-start rounded ">
-            <h3 className="font-bold">내 장소</h3>
-            {userDatas.map((place) => (
-              <UserInfo
-                key={`${place.id}: ${place.place_name}`}
-                name={place.place_name}
-              />
-            ))}
-            <div className="flex justify-around ">
-              <Paging className="rounded-md bg-mint px-3 py-1" />
-            </div>
+        <div className="flex justify-around">
+          <div className="w-4/5 flex-col items-center justify-start rounded md:w-[65vw]">
+            <h3 className="text-center font-bold">⭐️마이 북마크⭐️</h3>
+            {userDatas
+              .slice(items * (page - 1), items * (page - 1) + items)
+              .map((place) => (
+                <UserInfo
+                  key={`${place.id}: ${place.place_name}`}
+                  name={place.place_name}
+                  roadAddress={place.roadAddress}
+                />
+              ))}
+            {isLoading &&
+              new Array(10).fill(<UserInfo isLoading={isLoading} />)}
+            <Pagination
+              activePage={page}
+              itemsCountPerPage={items}
+              totalItemsCount={userDatas.length}
+              pageRangeDisplayed={5}
+              onChange={handlePageChange}
+              innerClass="flex w-full justify-around"
+              activeClass="bg-mint/80"
+              itemClass="px-2 rounded-md"
+            ></Pagination>
           </div>
-          <div className="w-[30vw] items-center justify-start rounded bg-white">
+          <div className="hidden w-[30vw] items-center justify-start rounded bg-white md:block">
             <div className="m-3 grid grid-cols-10 justify-around gap-4 text-center">
               <div className="col-span-2 rounded-md bg-mint">태그</div>
               <div className="col-span-2 rounded-md bg-mint">태그</div>
--- components/userinfo.jsx
+++ components/userinfo.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import Spinner from '@/components/svg/Spinner';
 
-const UserInfo = ({ name, roadAddress, id }) => {
+const UserInfo = ({ name, roadAddress, isLoading }) => {
   return (
-    <div className="my-2 flex h-[5vh] w-full flex-col items-center justify-center rounded-md bg-white">
-      <div className="grid grid-cols-10 justify-around gap-6 text-center">
-        <div className="col-span-3 col-start-1 text-mint">{name}</div>
-        <div className="col-span-4 col-start-4 text-mint">{roadAddress}</div>
-        <div className="col-span-2 col-start-8 text-mint">{id}</div>
-        <button className="col-start-10 w-[3vw] rounded bg-mint text-white">
-          삭제
-        </button>
-      </div>
+    <div
+      className={`my-2 flex h-[5vh] w-full items-center justify-start overflow-hidden rounded-md bg-white`}
+    >
+      {name && (
+        <>
+          <div className="mx-2 w-1/5 flex-grow text-mint">{name}</div>
+          <div className="hidden w-1/3 flex-grow text-gray/80 md:inline-block">
+            {roadAddress}
+          </div>
+          <button className="mx-2 flex-none rounded border border-mint px-1">
+            삭제
+          </button>
+        </>
+      )}
+      {isLoading && (
+        <div className="flex h-full w-full animate-pulse items-center justify-center bg-gradient-to-br from-white via-mint/30 to-mint/50">
+          <Spinner />
+        </div>
+      )}
     </div>
   );
 };
